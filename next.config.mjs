c620a5dc29b24d7d0e260ade3a8c4f24558f1c64
Asjad Nasim <asjadnasim@gmail.com>
2024-05-24 14:01:31 +0530

    remove

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	// Customize webpack configuration
-	webpack: (config, { isServer }) => {
-		// Modify webpack config
-		return config;
-	},
-
-	// Define custom routes
-	exportPathMap: async function () {
-		return {
-			'/': { page: '/' },
-		};
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
